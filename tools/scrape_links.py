c1a157030535b528977bd88f876a6f90a1480aa4
SpaceMonkeyy86 <jackreynolds9128@gmail.com>
2024-10-26 14:49:22 -0400

    Add college information for major links

--- tools/scrape_links.py
+++ tools/scrape_links.py
@@ -16,17 +16,27 @@ for element in soup.find_all("h2"):
         root_div = element.next_sibling.next_sibling
         break
 
-for element in root_div.find_all("li"):
-    if not element.a:
-        continue
-    element = element.a
+colleges = {}
 
-    name = element.text
-    url = "https://catalog.purdue.edu/" + element["href"]
-    output.append({"name": name, "url": url})
+for element in root_div.children:
+    if element.name == "h3":
+        college = element.text.strip()
+        div = element.next_sibling.next_sibling
+        if div and div.name == "div":
+            colleges[college] = div
+
+for college, div in colleges.items():
+    for element in div.find_all("li"):
+        if not element.a:
+            continue
+        element = element.a
+    
+        name = element.text
+        url = "https://catalog.purdue.edu/" + element["href"]
+        output.append({"name": name, "college": college, "url": url})
 
 with open("links.csv", "w") as file:
-    writer = csv.DictWriter(file, fieldnames=["name", "url"])
+    writer = csv.DictWriter(file, fieldnames=["name", "college", "url"])
     writer.writeheader()
     for program in output:
         writer.writerow(program)
